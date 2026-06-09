28c7ec8f8e31eae7bf6a9f2f6c5ba40d3a83c5b3
Matthew Barker <matthewbarker@librem.one>
2026-06-09 20:07:07 +0100

    test: scaffold BIS.PBO.Test project

---
+++ BIS.PBO.Test/PboTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using BIS.PBO;
+using System.IO;
+
+namespace BIS.PBO.Test
+{
+    public class PboTests
+    {
+        [Fact]
+        public void Pbo_Structure_ShouldBeLoadable()
+        {
+            // Placeholder: Test if an empty or minimal PBO structure can be instantiated.
+            // A real test would require a sample PBO file.
+            Assert.True(true);
+        }
+    }
+}

d7dd9fc1a44926e6401739ec3b26918fe8050d01
Matthew Barker <matthewbarker@librem.one>
2026-06-09 20:52:46 +0100

    test: scaffold tests for ALB and PAA.Encoder

---
+++ BIS.ALB.Test/Core/AlbTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using Xunit;
+using BIS.ALB;
+using BIS.Core.Streams;
+
+namespace BIS.ALB.Test.Core
+{
+    public class AlbTest
+    {
+        [Fact]
+        public void Alb_CanInitialize()
+        {
+            // Simple verification test
+            Assert.True(true);
+        }
+    }
+}
---
+++ BIS.PAA.Encoder.Test/Format/PaaEncoderTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using BIS.PAA.Encoder;
+
+namespace BIS.PAA.Encoder.Test.Format
+{
+    public class PaaEncoderTest
+    {
+        [Fact]
+        public void Encoder_ShouldExist()
+        {
+            // PaaEncoder is static, cannot be instantiated.
+            Assert.True(true);
+        }    }
+}

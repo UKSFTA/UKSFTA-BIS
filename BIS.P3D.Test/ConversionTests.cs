51ef678c71fff9b6862603826f9737c1e667d708
Matthew Barker <matthewbarker@librem.one>
2026-06-09 20:03:21 +0100

    test: add placeholder conversion test in BIS.P3D

---
+++ BIS.P3D.Test/ConversionTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using BIS.P3D.ODOL;
+using BIS.P3D.MLOD;
+using BIS.P3D.Conversion;
+using BIS.Core.Math;
+
+namespace BIS.P3D.Test
+{
+    public class ConversionTests
+    {
+        [Fact]
+        public void ODOL2MLOD_Convert_ShouldExist()
+        {
+            Assert.True(true);
+        }
+    }
+}

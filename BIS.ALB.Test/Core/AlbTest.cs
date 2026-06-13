ffe6ce0128c93482e306982d16f8cbd8b1215d41
Matthew Barker <matthewbarker@librem.one>
2026-06-13 21:35:12 +0100

    test(alb): update ALB tests

--- BIS.ALB.Test/Core/AlbTest.cs
+++ BIS.ALB.Test/Core/AlbTest.cs
@@ -1,18 +1,46 @@
 using System;
 using System.IO;
-using Xunit;
-using BIS.ALB;
 using BIS.Core.Streams;
+using Xunit;
 
 namespace BIS.ALB.Test.Core
 {
     public class AlbTest
     {
         [Fact]
-        public void Alb_CanInitialize()
+        public void Constructor_InvalidSignature_ThrowsFormatException()
         {
-            // Simple verification test
-            Assert.True(true);
+            using var ms = new MemoryStream();
+            var writer = new BinaryWriterEx(ms);
+            writer.WriteAscii("XXXX", 4);
+            writer.Write(0);
+            writer.Flush();
+            ms.Position = 0;
+
+            var reader = new BinaryReaderEx(ms);
+            Assert.Throws<FormatException>(() => new ALB1(reader));
+        }
+
+        [Fact]
+        public void Constructor_EmptyStream_Throws()
+        {
+            using var ms = new MemoryStream();
+            var reader = new BinaryReaderEx(ms);
+            Assert.ThrowsAny<Exception>(() => new ALB1(reader));
+        }
+
+        [Fact]
+        public void Constructor_NullReader_ThrowsNullReferenceException()
+        {
+            Assert.Throws<NullReferenceException>(() => new ALB1(default(BinaryReaderEx)));
+        }
+
+        [Fact]
+        public void Constructor_TooShortStream_ThrowsEndOfStream()
+        {
+            using var ms = new MemoryStream(new byte[] { (byte)'A' });
+            var reader = new BinaryReaderEx(ms);
+            Assert.Throws<EndOfStreamException>(() => new ALB1(reader));
         }
     }
 }

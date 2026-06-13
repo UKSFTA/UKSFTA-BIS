301582bfb00088d196c89764f60d8972af3872ae
Matthew Barker <matthewbarker@librem.one>
2026-06-13 21:35:32 +0100

    test(paa/encoder): update PAA encoder tests

--- BIS.PAA.Encoder.Test/Format/PaaEncoderTest.cs
+++ BIS.PAA.Encoder.Test/Format/PaaEncoderTest.cs
@@ -1,15 +1,73 @@
+using System;
+using System.IO;
+using BCnEncoder.Shared;
+using Microsoft.Toolkit.HighPerformance;
+using BIS.Core.Streams;
+using BIS.PAA;
 using Xunit;
-using BIS.PAA.Encoder;
 
 namespace BIS.PAA.Encoder.Test.Format
 {
     public class PaaEncoderTest
     {
         [Fact]
-        public void Encoder_ShouldExist()
+        public void WritePAA_UnsupportedType_ThrowsNotSupportedException()
         {
-            // PaaEncoder is static, cannot be instantiated.
-            Assert.True(true);
+            var image = new ColorRgba32[4, 4];
+            for (int y = 0; y < 4; y++)
+                for (int x = 0; x < 4; x++)
+                    image[y, x] = new ColorRgba32(255, 0, 0, 255);
+
+            using var ms = new MemoryStream();
+            var writer = new BinaryWriterEx(ms);
+
+            Assert.Throws<NotSupportedException>(() =>
+                PaaEncoder.WritePAA(writer, new ReadOnlyMemory2D<ColorRgba32>(image),
+                    new ColorRgba32(255, 0, 0, 255), new ColorRgba32(128, 0, 0, 128),
+                    PAAType.DXT3, PAAFlags.InterpolatedAlpha));
+        }
+
+        [Fact]
+        public void WritePAA_ConvenienceMethod_WritesToFile()
+        {
+            var image = new ColorRgba32[8, 8];
+            for (int y = 0; y < 8; y++)
+                for (int x = 0; x < 8; x++)
+                    image[y, x] = new ColorRgba32(255, 0, 0, 255);
+
+            var path = Path.GetTempFileName();
+            try
+            {
+                PaaEncoder.WritePAA(path, image, PAAType.DXT1, PAAFlags.InterpolatedAlpha);
+                var fileInfo = new FileInfo(path);
+                Assert.True(fileInfo.Length > 100, $"PAA file too short: {fileInfo.Length}");
+            }
+            finally
+            {
+                File.Delete(path);
+            }
+        }
+
+        [Fact]
+        public void WritePAA_AutoDetect_OpaqueUsesDXT1()
+        {
+            // All opaque pixels (alpha=255) → auto-detect DXT1
+            var image = new ColorRgba32[16, 16];
+            for (int y = 0; y < 16; y++)
+                for (int x = 0; x < 16; x++)
+                    image[y, x] = new ColorRgba32(128, 200, 50, 255);
+
+            var path = Path.GetTempFileName();
+            try
+            {
+                PaaEncoder.WritePAA(path, image);
+                var fileInfo = new FileInfo(path);
+                Assert.True(fileInfo.Length > 100, $"PAA file too short: {fileInfo.Length}");
+            }
+            finally
+            {
+                File.Delete(path);
+            }
         }
     }
 }

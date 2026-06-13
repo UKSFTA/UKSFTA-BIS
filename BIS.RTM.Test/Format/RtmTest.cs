dd71fd89e9dd953947a57ee68339a653fb438668
Matthew Barker <matthewbarker@librem.one>
2026-06-13 21:35:42 +0100

    test(rtm/wrp): update RTM and WRP tests

--- BIS.RTM.Test/Format/RtmTest.cs
+++ BIS.RTM.Test/Format/RtmTest.cs
@@ -1,16 +1,60 @@
+using System;
+using System.IO;
 using Xunit;
-using BIS.RTM;
 
 namespace BIS.RTM.Test
 {
     public class RtmTest
     {
         [Fact]
-        public void Rtm_CanInitialize()
+        public void Constructor_InvalidSignature_ThrowsFormatException()
         {
-            // Cannot initialize empty RTM. 
-            // Just verify the placeholder runs.
-            Assert.True(true);
+            using var ms = new MemoryStream();
+            var writer = new BIS.Core.Streams.BinaryWriterEx(ms);
+            writer.WriteAscii("INVALID!", 8);
+            writer.Flush();
+            ms.Position = 0;
+
+            Assert.Throws<FormatException>(() => new RTM(ms));
+        }
+
+        [Fact]
+        public void Constructor_EmptyStream_Throws()
+        {
+            using var ms = new MemoryStream();
+            Assert.ThrowsAny<Exception>(() => new RTM(ms));
+        }
+
+        [Fact]
+        public void Constructor_PartialSignature_Throws()
+        {
+            using var ms = new MemoryStream();
+            var writer = new System.IO.BinaryWriter(ms);
+            writer.Write("RTM_"u8);
+            writer.Flush();
+            ms.Position = 0;
+
+            Assert.ThrowsAny<Exception>(() => new RTM(ms));
+        }
+
+        [Fact]
+        public void Constructor_ValidMinimal_CreatesInstance()
+        {
+            using var ms = new MemoryStream();
+            var writer = new BIS.Core.Streams.BinaryWriterEx(ms);
+            writer.WriteAscii("RTM_0101", 8);
+            writer.Write(0.0f); // displacement.x
+            writer.Write(0.0f); // displacement.y
+            writer.Write(0.0f); // displacement.z
+            writer.Write(0);    // 0 frames
+            writer.Write(0);    // 0 bones
+            writer.Flush();
+            ms.Position = 0;
+
+            var rtm = new RTM(ms);
+            Assert.NotNull(rtm);
+            Assert.Empty(rtm.BoneNames);
+            Assert.Empty(rtm.FrameTimes);
         }
     }
 }
--- BIS.WRP.Test/Format/WrpTest.cs
+++ BIS.WRP.Test/Format/WrpTest.cs
@@ -1,15 +1,40 @@
+using System;
+using System.IO;
+using BIS.Core.Streams;
 using Xunit;
-using BIS.WRP;
 
 namespace BIS.WRP.Test
 {
     public class WrpTest
     {
         [Fact]
-        public void Wrp_CanInitialize()
+        public void Constructor_Default_IsNotNull()
         {
             var wrp = new AnyWrp();
             Assert.NotNull(wrp);
+        }
+
+        [Fact]
+        public void Read_UnknownSignature_ThrowsInvalidOperationException()
+        {
+            using var ms = new MemoryStream();
+            var writer = new BinaryWriterEx(ms);
+            writer.WriteAscii("XXXX", 4);
+            writer.Flush();
+            ms.Position = 0;
+
+            var wrp = new AnyWrp();
+            Assert.Throws<InvalidOperationException>(() => wrp.Read(new BinaryReaderEx(ms)));
+        }
+
+        [Fact]
+        public void Read_EmptyStream_Throws()
+        {
+            using var ms = new MemoryStream();
+            var reader = new BinaryReaderEx(ms);
+
+            var wrp = new AnyWrp();
+            Assert.ThrowsAny<Exception>(() => wrp.Read(reader));
         }
     }
 }
